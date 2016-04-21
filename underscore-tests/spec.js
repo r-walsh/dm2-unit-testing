626bdbf5e88bf9290ca85de4e7c2ccf4b576a286
Ryan Walsh <ryan@thehatrack.net>
2016-04-21 09:26:16 -0500

    checking out previous commit

--- underscore-tests/spec.js
+++ underscore-tests/spec.js
@@ -1,24 +1,12 @@
-describe('_.first', function() {
+'use strict';
 
-	it('be able to return the first element of an array if n is undefined', function() {
-		expect(_.first([1, 2, 3])).to.equal(1);
-		expect(_.first([2])).to.equal(2);
+describe('_.first', () => {
+	it(`returns the first value in an array if n is undefined`, () => {
+		expect( _.first( [1, 2, 3] )).to.equal(1);
+		expect( _.first( [3] )).to.equal(3);
 	});
 
-	it('should return the first N elements of an array', function() {
-		expect(_.first([1, 2, 3, 4, 5], 3)).to.eql([1, 2, 3])
+	it(`returns the first n elements of an array`, () => {
+		expect( _.first( [1, 2, 3, 4, 5], 3 )).to.eql([1, 2, 3]);
 	});
-
-});
-
-describe('_.indexOf', function() {
-
-	it('returns the index of the target value', function() {
-		expect(_.indexOf([1, 2, 3], 2)).to.equal(1);
-	});
-
-	it('should return -1 if the target value is not found', function() {
-		expect(_.indexOf([1, 2, 3], 4)).to.equal(-1);
-	});
-
 });
