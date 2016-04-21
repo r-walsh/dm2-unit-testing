eb04a6ec151e3a55839faec697d65077832c97ee
Ryan Walsh <ryan@thehatrack.net>
2016-04-21 09:30:30 -0500

    Update homeCtrlSpec.js

--- angular-tests/spec/homeCtrlSpec.js
+++ angular-tests/spec/homeCtrlSpec.js
@@ -1,19 +1,1 @@
-'use strict';
 
-describe(`homeCtrl`, () => {
-	let $controller;
-
-	beforeEach(module(`ngTests`));
-
-	beforeEach(inject( _$controller_ => {
-		$controller = _$controller_;
-	}));
-
-	describe(`$scope.returnOne`, () => {
-		it(`returns the number one`, () => {
-			let controller = $controller(`homeCtrl`, { $scope: {} });
-
-			expect( controller.scope.returnOne() ).to.equal(1);
-		});
-	});
-});
